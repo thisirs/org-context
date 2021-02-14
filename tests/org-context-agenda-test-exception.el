57a4af30902d77b4008e2179dbfa309dd58a74aa
thisirs <thisirs@gmail.com>
2021-02-14 14:23:56 +0100

    Reflect recent changes in tests

--- tests/org-context-agenda-test-exception.el
+++ tests/org-context-agenda-test-exception.el
@@ -2,25 +2,23 @@
 
 (ert-deftest org-context-advice ()
   "Tests if `org-agenda' and `org-capture' are advised."
-  (should (ad-is-advised 'org-agenda))
-  (should (ad-is-advised 'org-capture))
-  (should-not (ad-is-active 'org-agenda))
-  (should-not (ad-is-active 'org-capture))
 
-  (org-context-activate 1)
+  (should-not (advice-member-p 'org-capture-advice 'org-capture))
+  (should-not (advice-member-p 'org-agenda-advice 'org-agenda))
 
-  (should (ad-is-active 'org-agenda))
-  (should (ad-is-active 'org-capture))
+  (org-context-mode +1)
+  (should (advice-member-p 'org-capture-advice 'org-capture))
+  (should (advice-member-p 'org-agenda-advice 'org-agenda))
 
-  (org-context-activate -1)
-  (should-not (ad-is-active 'org-agenda))
-  (should-not (ad-is-active 'org-capture)))
+  (org-context-mode -1)
+  (should-not (advice-member-p 'org-capture-advice 'org-capture))
+  (should-not (advice-member-p 'org-agenda-advice 'org-agenda)))
 
 (ert-deftest org-context-agenda-check-local ()
   "Tests if `org-context-agenda' is local."
   (let ((directory (expand-file-name "ProjectA" resources-path))
         buffer)
-    (org-context-activate 1)
+    (org-context-mode +1)
     (setq buffer (find-file-noselect (expand-file-name "foo" directory)))
     (with-current-buffer buffer
       (should (local-variable-p 'org-context-agenda)))))
@@ -30,7 +28,7 @@
   (let ((directory (expand-file-name "ProjectA" resources-path))
         buffer)
     (cl-letf (((symbol-function 'yes-or-no-p) (lambda (foo) t)))
-      (org-context-activate)
+      (org-context-mode +1)
       (setq buffer (find-file-noselect (expand-file-name "foo" directory)))
       (with-current-buffer buffer
         (should (local-variable-p 'org-context-agenda))
